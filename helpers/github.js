65e5e01b86223760c59904a366bc07ece2ad525f
Evan <evantatkinson@gmail.com>
2018-01-02 20:08:10 -0800

    working on github.js

--- helpers/github.js
+++ helpers/github.js
@@ -1,19 +1,24 @@
 const request = require('request');
 const config = require('../config.js');
 
-let getReposByUsername = (/* TODO */) => {
-  // TODO - Use the request module to request repos for a specific
-  // user from the github API
-
-  // The options object has been provided to help you out, 
-  // but you'll have to fill in the URL
+let getReposByUsername = (username) => {
+  
   let options = {
-    url: 'FILL ME IN',
+    url: `https://api.github.com/${users}`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
+  // TODO - Use the request module to request repos for a specific
+  // user from the github API
+  request(options, function(err, res, body) {
+      let json = json.parse(body);
+      console.log(json)
+  })
+
+  // The options object has been provided to help you out, 
+  // but you'll have to fill in the URL
 
 }
 
